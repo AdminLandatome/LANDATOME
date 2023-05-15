63fbeddbdc77d1cba722b51c9d0a2836c1660807
AdminLandatome <133687374+AdminLandatome@users.noreply.github.com>
2023-05-15 22:11:46 +0200

    Add files via upload

---
+++ docs/01_premiere/014_st2s/04_Fonctions_part2/04_fonctions_part2_E01_ex3.py
@@ -0,0 +1,9 @@
+def taux_de_variation(f,x2,x2):
+   """retourne le taux de variation de 
+   la fonction f entre les valeurs x1 et x2"""
+   return ...
+   
+def f(x):
+    return x**2
+
+print(taux_de_variation(f,1,5))
