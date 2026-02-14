714fedfe72e9c0f7a20ac1ac6c0df3c13d77d871
AdminLandatome <133687374+AdminLandatome@users.noreply.github.com>
2026-02-14 11:05:32 +0100

    Add files via upload

---
+++ docs/python/06_variables_aleatoires_discretes_E01_ex5_script.py
@@ -0,0 +1,23 @@
+import random
+
+def simulation_gain():
+    de = random.randint(1, 6) # On lance un dé à 6 faces
+    
+    if de == 1:
+        return 20
+    elif de > 4:
+        return ... # Perte de 5 euros
+    elif de == 3:
+        return ... # Perte de 2 euros
+    else:
+        return ... # Gain de 1 euro
+
+# Calcul de la moyenne sur 10 000 parties
+nb_parties = 10000
+somme_gains = 0
+
+for i in range(nb_parties):
+    somme_gains = somme_gains + simulation_gain()
+
+moyenne = somme_gains / nb_parties
+print("Le gain moyen sur 10 000 parties est :", moyenne)
