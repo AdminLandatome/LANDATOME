077bcec9a12b808dc4432f958c790f11fbe688be
AdminLandatome <133687374+AdminLandatome@users.noreply.github.com>
2026-02-14 08:28:04 +0100

    Add files via upload

--- docs/python/06_variables_aleatoires_discretes_TP01_correction.py
+++ docs/python/06_variables_aleatoires_discretes_TP01_correction.py
@@ -1,11 +1,13 @@
 tailles = [10, 15, 20]
+tailles_mn = [100, 150, 200]  #cette ligne a été ajoutée
+tailles_boost = [15, 20, 25]  #cette ligne a été ajoutée
 probabilites = [0.6, 0.3, 0.1]
 
 def esperance(L_valeurs, L_probabilites):
     e = 0
     for i in range(len(L_valeurs)):
         # À COMPLÉTER : Formule de l'espérance
-        e = e + L_valeurs[i] * L_probabilites[i]
+        e = e + L_valeurs[i] * L_probabilites[i]  #cette ligne a été complétée
     return e
 
 def variance(L_valeurs, L_probabilites):
@@ -13,9 +15,10 @@ def variance(L_valeurs, L_probabilites):
     v = 0
     for i in range(len(L_valeurs)):
         # À COMPLÉTER : Moyenne des carrés des écarts
-        v = v + L_probabilites[i] * (L_valeurs[i] - e)**2
+        v = v + L_probabilites[i] * (L_valeurs[i] - e)**2  #cette ligne a été complétée
     return v
 
 # Résultats pour X
-print("X (en cm) : E(X) =", esperance(tailles, probabilites))
-print("X (en cm) : V(X) =", variance(tailles, probabilites))
+print("X (en cm) : E(X) =", esperance(tailles_boost, probabilites))   #par exemple, ici tailles est devenue tailles_boost
+print("X (en cm) : V(X) =", variance(tailles_boost, probabilites))    #par exemple, ici tailles est devenue tailles_boost
+
