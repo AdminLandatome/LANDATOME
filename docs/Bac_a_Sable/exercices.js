ecf69ae7aafa60e11afe32ff4d33b8360c3639ac
AdminLandatome <133687374+AdminLandatome@users.noreply.github.com>
2026-03-23 20:30:28 +0100

    Add files via upload

---
+++ docs/Bac_a_Sable/exercices.js
@@ -0,0 +1,10 @@
+const exercices = [
+    {
+        id: 1,
+        titre: "Bac à sable",
+        consigne: "Vous pouvez utiliser l'éditeur comme bon vous semble.",
+        codeDepart: ``,
+        test: "True==True" // Ce qui doit être vrai en Python
+    }
+	
+];
